b569833b208cde85b65ca24675fc798e80697499
Gustavo Godoi <gugodoi@gmail.com>
2018-03-14 12:02:39 -0300

    New channel name validation

--- next/src/modules/channel/containers/NewChannelContainer.js
+++ next/src/modules/channel/containers/NewChannelContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { func } from 'prop-types'
 import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
+import { noSpecialCharacter } from 'app/lib/form/validation'
 
 const mutation = gql`
   mutation CreateChannel ($name: String!) {
@@ -21,12 +22,23 @@ const mutation = gql`
 
 // @TODO: implement optimistic query on channels?
 
-const NewMessageContainer = ({ children }) => (
+const NewMessageContainer = ({ channels, children }) => (
   <Mutation mutation={ mutation } refetchQueries={ ['Channels'] }>
     { mutate => (
       children(name => {
         if (name) {
-          mutate({ variables: { name } })
+          if (channels.find(channel => channel.name === name)) {
+            window.alert(`There is already a channel called ${name}! O.o`)
+          }
+          else {
+            const test = noSpecialCharacter(name)
+            if (test) {
+              window.alert(`${test}`)
+            }
+            else {
+              mutate({ variables: { name } })
+            }
+          }
         }
       })
     ) }
@@ -35,6 +47,7 @@ const NewMessageContainer = ({ children }) => (
 
 NewMessageContainer.propTypes = {
   children: func,
+  channels: Array
 }
 
 export default NewMessageContainer
